7d034cfb0ca2f040bff3b4bdcbbdaade6b15d7a6
Sergei Chernov <goldengrail@gmail.com>
2024-08-25 17:48:13 +0500

    Alyshev Lesson 3

---
+++ src/main/java/ru/chernov/spring/TestSpring.java
@@ -0,0 +1,13 @@
+package ru.chernov.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean bean = context.getBean("testBean", TestBean.class);
+        System.out.println(bean.getName());
+        context.close();
+    }
+}
