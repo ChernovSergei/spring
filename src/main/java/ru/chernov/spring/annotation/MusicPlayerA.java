40d467055da17ebb70387b9b7b45f0d95af97705
Sergei Chernov <goldengrail@gmail.com>
2024-09-02 20:06:12 +0400

    Alyshev Lesson 10 Autowired 2

--- src/main/java/ru/chernov/spring/annotation/MusicPlayerA.java
+++ src/main/java/ru/chernov/spring/annotation/MusicPlayerA.java
@@ -1,11 +1,13 @@
 package ru.chernov.spring.annotation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayerA {
     private JazzMusic music;
 
+    @Autowired
     public MusicPlayerA(JazzMusic music) {
         this.music = music;
     }
