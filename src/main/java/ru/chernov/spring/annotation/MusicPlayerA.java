6f2d69f315a69f9be16bc5ec1fc7f04976acbfe3
Sergei Chernov <goldengrail@gmail.com>
2024-09-04 13:13:51 +0400

    Alyshev Lesson 11 Autowired 7

--- src/main/java/ru/chernov/spring/annotation/MusicPlayerA.java
+++ src/main/java/ru/chernov/spring/annotation/MusicPlayerA.java
@@ -1,20 +1,16 @@
 package ru.chernov.spring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayerA {
-    private JazzMusic jazzMusic;
-    private RapMusic rapMusic;
-
     @Autowired
-    public MusicPlayerA(JazzMusic jazzMusic, RapMusic rapMusic) {
-        this.jazzMusic = jazzMusic;
-        this.rapMusic = rapMusic;
-    }
+    @Qualifier("jazzMusic")
+    private MusicA music;
 
     public String playMusic() {
-        return "Playing " + jazzMusic.getSong() + " and playing " + rapMusic.getSong();
+        return "Playing " + music.getSong();
     }
 }
